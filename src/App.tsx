99299822e605d303c9e329ec78f54c1f1faeb4a8
abdmmar <opowae909@gmail.com>
2022-02-08 22:42:54 +0700

    feat: use Hero and add temp Main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,59 @@
 import * as React from 'react';
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
+import Hero from '@/components/Hero';
 import Button from '@/components/Button';
 
-const Main = Styled.main`
+const Main = styled.main`
   display: grid;
   place-items: center;
-  width:100%;
+  width: 100%;
   height: 100%;
+  padding: 50px 100px;
 `;
 
-const H = Styled.h1`
-  --font-size: 1.6rem;
-  --font-family: 'Roboto', sans-serif;
-  --font-color: #333;
+const MainContainer = styled.div`
+  position: relative;
+  width: 100%;
+`;
 
-  font-size: var(--font-size);
-  color: var(--font-color);
-  font-family: var(--font-family);
+const MainWrapper = styled.div`
+  top: -230px;
+  position: absolute;
+  background-color: white;
+  border-radius: 6px;
+  padding: 32px;
+  width: 100%;
+`;
+
+const MainContent = styled.div`
+  display: grid;
+  grid-row: column;
+  gap: 1rem;
 `;
 
 const App = () => {
   return (
-    <Main>
-      <H>Hello world!</H>
-      <Button></Button>
-    </Main>
+    <>
+      <Hero />
+      <Main>
+        <MainContainer>
+          <MainWrapper>
+            <MainContent>
+              <h3>Test</h3>
+              <ul>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+              </ul>
+              <Button></Button>
+            </MainContent>
+          </MainWrapper>
+        </MainContainer>
+      </Main>
+    </>
   );
 };
 
